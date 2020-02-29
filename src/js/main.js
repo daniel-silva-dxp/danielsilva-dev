a6e253a0b18117f7363f4b207e88de20e79bb65b
Daniel Silva <dani3l.dxp@gmail.com>
2020-02-29 12:04:31 -0300

    feat: aplicando animação e interação com click no menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,1 @@
 ('use strict');
-const navIcon = document.querySelector('.icon-menu');
-const sidebar = document.querySelector('.sidebar');
-
-navIcon.addEventListener('click', () => {
-	sidebar.classList.toggle('open');
-});
